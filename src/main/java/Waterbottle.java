c978d97358cac48ee85bc09f2d63d2dbc6d58e4e
Patrick Cullen <patrick47cullen@gmail.com>
2018-04-30 18:11:04 +0100

    First four tests passed for Waterbottle

---
+++ src/main/java/Waterbottle.java
@@ -0,0 +1,28 @@
+public class Waterbottle {
+    private String name;
+    private double volume;
+
+    public Waterbottle(String name, double volume) {
+        this.name = name;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getVolume() {
+        return this.volume;
+    }
+
+    public double Drink() {
+        this.volume -= 10;
+        return this.volume;
+    }
+
+    public double Empty() {
+        this.volume = 0;
+        return this.volume;
+    }
+
+}
---
+++ src/test/java/WaterbottleTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterbottleTest {
+
+    private Waterbottle testWaterbottle;
+
+    @Before
+    public void before() {
+        testWaterbottle = new Waterbottle("Waterbottle", 100);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Waterbottle", testWaterbottle.getName());
+    }
+
+    @Test
+    public void hasVolume() {
+        assertEquals(100, testWaterbottle.getVolume(), 0.01);
+    }
+
+    @Test
+    public void doesDrink() {
+        assertEquals(90, testWaterbottle.Drink(), 0.01);
+    }
+
+    @Test
+    public void isEmpty() {
+        assertEquals(0, testWaterbottle.Empty(), 0.01);
+    }
+}
